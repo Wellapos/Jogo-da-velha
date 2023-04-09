f25b965225fed06b0fcbbdc06a4e115b1ca59f1b
wellapos <wellington258@live.com>
2023-04-09 17:15:31 -0300

    fix: hm

--- src/hooks/useDatabase.ts
+++ src/hooks/useDatabase.ts
@@ -1,27 +1,34 @@
-import { OnlineGameProps } from "../interfaces";
-import { database } from "../services";
+import { OnlineGameProps } from '../interfaces'
+import { database } from '../services'
 
 export interface UpdateGameProps {
-  id: string;
-  game: OnlineGameProps;
+  id: string
+  game: OnlineGameProps
 }
 
 export const useDatabase = () => {
   const createGame = async (game: OnlineGameProps) => {
-    const gameRef = database.ref("games");
+    const gameRef = database.ref('games')
 
-    return await gameRef.push(game);
-  };
+    return await gameRef.push(game)
+  }
 
   const updateGame = async ({ id, game }: UpdateGameProps) => {
-    const gameRef = database.ref(`games/${id}`);
+    const gameRef = database.ref(`games/${id}`)
 
-    return await gameRef.update(game);
-  };
+    return await gameRef.update(game)
+  }
 
   const getGameRefById = (id: string) => {
-    return database.ref(`games/${id}`);
-  };
+    return database.ref(`games/${id}`)
+  }
 
-  return { createGame, updateGame, getGameRefById };
-};
+  const leaveGame = (id: string) => {
+    const gameRef = database.ref(`games/${id}`)
+
+    gameRef.onDisconnect().remove()
+    gameRef.remove()
+  }
+
+  return { createGame, updateGame, getGameRefById, leaveGame }
+}
--- src/hooks/useOnlineGame.ts
+++ src/hooks/useOnlineGame.ts
@@ -1,55 +1,57 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
-import { GameProps, OnlineGameProps, PlaceOptions } from "../interfaces";
-import { useDatabase } from "./useDatabase";
-import { useGame } from "./useGame";
+import { GameProps, OnlineGameProps, PlaceOptions } from '../interfaces'
+import { useDatabase } from './useDatabase'
+import { useGame } from './useGame'
 
 export interface UseOnlineGameReturn {
-  game: OnlineGameProps;
-  updateBoardAndTurn: (place: PlaceOptions) => void;
-  handleRestartGame: () => void;
+  game: OnlineGameProps
+  updateBoardAndTurn: (place: PlaceOptions) => void
+  handleRestartGame: () => void
 }
 
 export const useOnlineGame = (id?: string): UseOnlineGameReturn => {
   const defaultGame: GameProps = {
-    board: ["", "", "", "", "", "", "", "", ""],
-    turn: "x",
-  };
+    board: ['', '', '', '', '', '', '', '', ''],
+    turn: 'x'
+  }
 
-  const [game, setGame] = useState<OnlineGameProps>(defaultGame);
-  const { getGameRefById, updateGame } = useDatabase();
+  const [game, setGame] = useState<OnlineGameProps>(defaultGame)
+  const { getGameRefById, updateGame } = useDatabase()
 
   const updateGameState = async (gameState: GameProps) => {
-    if (!game || !game.firstPlayer || !game.secondPlayer || !id) return;
+    if (!game || !game.firstPlayer || !game.secondPlayer || !id) return
 
     await updateGame({
       id,
       game: {
         firstPlayer: {
-          ...game.firstPlayer,
+          ...game.firstPlayer
         },
         secondPlayer: {
-          ...game.secondPlayer,
+          ...game.secondPlayer
         },
-        ...gameState,
-      },
-    });
-  };
+        ...gameState
+      }
+    })
+  }
 
   useEffect(() => {
-    if (!id) return;
+    if (!id) return
 
-    const gameRef = getGameRefById(id);
+    const gameRef = getGameRefById(id)
 
-    gameRef.on("value", (snapshot) => {
-      const game = snapshot.val() as OnlineGameProps;
-      setGame(game);
-    });
+    gameRef.on('value', (snapshot) => {
+      const game = snapshot.val() as OnlineGameProps
+      setGame(game)
+    })
+
+    gameRef.onDisconnect().remove()
 
     return () => {
-      gameRef.off();
-    };
-  }, [game, id]);
+      gameRef.off()
+    }
+  }, [game, id])
 
-  return useGame({ game, setGame: updateGameState });
-};
+  return useGame({ game, setGame: updateGameState })
+}
